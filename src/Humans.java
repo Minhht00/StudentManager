40090f553778997f9c9473822e3bb6a330db560e
luong <luongtuanminh02161@gmail.com>
2023-05-15 09:53:07 +0700

    first

---
+++ src/Humans.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Humans {
+    private int id;
+    private String name;
+    private Date birthday;
+    private String address;
+    private double height;
+    private double weight;
+
+
+}
